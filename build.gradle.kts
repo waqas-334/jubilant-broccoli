d979082d54297747380952082d044b47892000d2
waqas-334 <waqasyounis334@gmail.com>
2024-09-05 17:01:00 -0230

    Removed comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,3 @@
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-//
-//buildscript {
-//    repositories {
-//        // other repositories...
-//        google()
-//        mavenCentral()
-//    }
-//    dependencies {
-//        // other plugins...
-//        classpath("com.android.tools.build:gradle:8.5.2")
-////        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.52")
-////        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
-////        classpath(libs.plugins.compose.compiler)
-//    }
-//}
-//
-
 buildscript {
     repositories {
         mavenCentral()
